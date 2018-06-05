7b7912b76262b5f33bd03100367bac1ce0682bda
ErickBSG <erickgarfiasbsg@gmail.com>
2018-06-05 14:44:56 -0500

    Primer Avance De Proyecto

--- src/sparrow/Principal.java
+++ src/sparrow/Principal.java
@@ -16,6 +16,7 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
+        //Ejemplo De GIT
         this.setLocationRelativeTo(null);
     }
 
